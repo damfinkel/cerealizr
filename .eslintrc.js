a2a15a157cb4eea7732909fc5a945c09b7618e43
damfinkel <damfinkel@gmail.com>
2018-08-25 18:59:09 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    browser: true
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
+    },
+    sourceType: "module"
+  },
+  plugins: ["prettier"],
+  extends: ["eslint:recommended"],
+  globals: {
+    __DEV__: true
+  },
+  rules: {
+    "prettier/prettier": ["error", { printWidth: 110, singleQuote: true }],
+    "no-invalid-this": "off",
+    "no-return-assign": "error",
+    "no-param-reassign": "error",
+    "no-nested-ternary": "error"
+  }
+};
